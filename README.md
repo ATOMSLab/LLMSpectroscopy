8be5c2e531b60a949ceaab97a27c637e22d76a5f
Kevin Ishimwe <ishimwekevin45@gmail.com>
2024-05-08 19:50:47 -0400

    update dataset.py can download

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # LLMSpectroscopy
 Automated spectroscopy analysis for the 2024 LLM Hackathon for Applications in Materials and Chemistry
+
+## Setting up the Project
+
+Follow these steps to set up the project on your local machine:
+
+### Prerequisites
+
+- Python 3.x
+- pip package manager
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/ATOMSLab/LLMSpectroscopy.git
+```
+2. Navigate to the project directory:
+```bash
+cd LLMSpectroscopy
+```
+3. Create and activate a virtual environment :
+```bash 
+python3 -m venv env
+source env/bin/activate  
+```
+4. Install the required packages:
+```bash 
+pip3 install -r requirements.txt
+```
+## Downloading Datasets
+
+To download the necessary datasets, run the following command:
+
+```bash 
+python3 dataset.py
+```
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- dataset.py
+++ dataset.py
@@ -1,32 +1,31 @@
 import requests
+import pandas as pd
 
-# essentially modify the code however you want and add the id and names of molecules
-molecule_options=[
-    {
-        "name":"C2H6",
-        "id":"C74840"
-    }
-    ]
-
+def load_list():
+    csv=pd.read_csv("CandidateList_HL.csv")
+    csv['CAS']='C'+csv['CAS'].str.replace('-', '').str.join('')
+    return  csv.to_dict('records')
 
 def downloadMolecules(molecule):
-    url=f"https://webbook.nist.gov/cgi/cbook.cgi?JCAMP={molecule['id']}&Index=1&Type=IR"
+    url=f"https://webbook.nist.gov/cgi/cbook.cgi?JCAMP={molecule['CAS']}&Index=1&Type=IR"
     response = requests.get(url,stream=True)
     if response.status_code == 200:
-        with open(f"./dataset/C287923", "wb") as file:
+        with open(f"./dataset/{molecule['molecular formula']}", "wb") as file:
             for chunk in response.iter_content(chunk_size=1024):
                 file.write(chunk)
-        print(f"{molecule['name']} download successfully!")
+        print(f"{molecule['molecular formula']} download successfully!")
     else:
         print(f"download failed. Status code: {response.status_code}")
 
 
-def getMolecules():
+def getMolecules(molecule_options):
+   
     for molecule in molecule_options:
-        try:
-            return downloadMolecules(molecule)
+        try: 
+            downloadMolecules(molecule)
         except Exception:
             print(Exception)
 
 if (__name__=="__main__"):
-    getMolecules()
+    molecules=load_list()
+    getMolecules(molecules)
