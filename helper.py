5cb0ffabdd9cd276a1a1cd051d435acd9079da88
Kevin Ishimwe <ishimwekevin45@gmail.com>
2024-06-04 21:12:19 -0400

    cross checking function

--- helper.py
+++ helper.py
@@ -1,11 +1,10 @@
 
 import requests
 from html.parser import HTMLParser
+import re
 
-
-
-#getIsomerSuggestion(C9H20O)
-def getIsomerSuggestion(molecule):
+#i.e et_isomer_suggestion(C9H20O)
+def get_isomer_suggestion(molecule):
  try:
     response=requests.get(f"https://webbook.nist.gov/cgi/cbook.cgi?Formula={molecule}&MatchIso=on&NoIon=on&Units=SI")
     if(response.status_code==200):
@@ -16,7 +15,10 @@ _________________________________________________\n\n The number of  possible Is
         ""","\033[0m","\n".join(suggestions),'\n_________________________________________________')
         return suggestions
  except Exception:
-        print("no suggestions available")      
+        print("no suggestions available") 
+        return []     
+
+# html cleaning and parsign
 
 def parse_html(content):
     parser = HTMLParser()
@@ -34,26 +36,39 @@ def parse_html_suggestions(content,molecule):
     return txt.split(f'({molecule})')
 
 
+def parse_html_other_other_names(name,content):
+    data= parse_html(content)
+    txt=''.join(data).split('Other names:')[1]
+    other_names=txt.split('Permanent link')[0].strip()
+    return other_names.replace("\n","").lower().split(';')
 
 
-
-
-
-# getOthernames("Acetic acid, butyl ester")
-def getOthernames(name):
+# get_other_names("Acetic acid, butyl ester")
+def get_other_names(name):
  try:
     c_name="%2C+".join(name.replace(',','').split(" "))
     url=f"https://webbook.nist.gov/cgi/cbook.cgi?Name={c_name}&Units=SI"
     response=requests.get(url)
     if (response.status_code==200):
-      parse_html_other_other_names(name,response.text)
+      return parse_html_other_other_names(name,response.text)
  except Exception:
     print("NO Other Names for ",name, "found")
+    return []
 
-def parse_html_other_other_names(name,content):
-    data= parse_html(content)
-    txt=''.join(data).split('Other names:')[1]
-    other_names=txt.split('Permanent link')[0].strip()
-    print(f"\033[94m_________________________________________________\n\n Other names for {name} compound include\n_________________________________________________\n","\033[0m",other_names,'\n_________________________________________________\n')
-    return other_names
+def clean_molecule_name(name):
+    cleaned_name = re.sub(r'[^a-zA-Z0-9\s]', '', name.replace("n-", ""))
+    return cleaned_name.replace(" ", "").lower()
+# cross_check_molecule("acetic acid n-butyl ester","acetic acid, butyl ester")
+def cross_check_molecule(llmgeneration, correct_molecule):
+    options=get_other_names(correct_molecule)
+    llmcleaned = clean_molecule_name(llmgeneration)
+    for molecule in options:
+        cleaned = clean_molecule_name(molecule)
+        if cleaned == llmcleaned:
+            print("\033[92mThe molecules were a match\033[0m")
+            print(llmgeneration,"\n",correct_molecule)
+            return True
 
+    print("\033[91mThe molecules were not a match\033[0m")
+    print(f"\033[94m_________________________________________________\n\n [INFO] llmgeneration: {llmgeneration} \n [INFO] Options of correct name :\n_________________________________________________\n","\033[0m","\n".join(options),'\n\033[94m_________________________________________________\033[0m\n')
+    return False
